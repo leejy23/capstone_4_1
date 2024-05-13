885c6f0dc0e32653c42a102ae62066ed8c6a3da5
Lee jae young <105205327+leejy23@users.noreply.github.com>
2024-05-13 13:53:20 +0900

    php,db정보 연동(location에 보이진 않음.)

--- php_Folder/view.php
+++ php_Folder/view.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>view</title>
-</head>
-<body>
-    <?php
+<?php
+include_once("connect.php");
+$sql = "SELECT * FROM search";
+$result = $conn->query($sql);
 
-    ?>
-</body>
-</html>
+if(isset($result) && $result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "이름: ". $row['name'];
+        echo "지역: ". $row['location']."<br>";
+        echo "형태: ". $row['type']."<br>";
+        echo "거래형태: ". $row['billtype']."<br>";
+        echo "크기: ". $row['size']."<hr><br>";;
+    }
+} else echo "검색된 데이터가 없습니다.";
+
+?>
