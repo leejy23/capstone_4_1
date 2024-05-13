2487445bf883cf67c8828a8a7e59f4e1cfb5c7da
Lee jae young <105205327+leejy23@users.noreply.github.com>
2024-05-13 15:23:45 +0900

    검색 시 정보 보여지도록 수정test3

--- php_Folder/view.php
+++ php_Folder/view.php
@@ -16,9 +16,9 @@
         while($row = $result->fetch_assoc()){
             echo "이름: ". $row['name']."<br>";
             echo "지역: ". $row['location']."<br>";
-            echo "형태: ". $row['type']."<br>";
-            echo "거래형태: ". $row['billtype']."<br>";
-            echo "크기: ". $row['size']."<hr><br>";;
+            echo "주거 형태: ". $row['type']."<br>";
+            echo "거래 형태: ". $row['billtype']."<br>";
+            echo "크기: ". $row['size']."㎡<hr><br>";;
         }
     } else echo "검색된 데이터가 없습니다.";
 
