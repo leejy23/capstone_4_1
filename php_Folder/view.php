dec6d92f41bfef1d4b1e1d4aa43632f0699fdc73
Lee jae young <105205327+leejy23@users.noreply.github.com>
2024-05-22 21:07:18 +0900

    검색 시 해당내용 포함 데이터 출력 구현(아파치 로컬 한정)

--- php_Folder/view.php
+++ php_Folder/view.php
@@ -3,25 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>부동산 검색 결과</title>
+    <style>
+        .center {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
 </head>
 <body>
-    <h1>검색결과</h1>
+    <h1>검색결과</h1> 
     <?php
     include_once("connect.php");
-    $sql = "SELECT * FROM search";
+    $search = isset($_GET['search']) ? $_GET['search'] : '';
+    // 검색어가 포함된 데이터를 검색하는 SQL 쿼리
+    $sql = "SELECT * FROM search WHERE name LIKE '%$search%' OR location LIKE '%$search%' OR type LIKE '%$search%' OR billtype LIKE '%$search%'";
+    $result = $conn->query($sql);
+    $total_results = $result->num_rows;
+    $results_per_page = 10;
+    $number_of_pages = ceil($total_results/$results_per_page);
+
+    if(!isset($_GET['page'])){
+        $page = 1;
+    } else {
+        $page = $_GET['page'];
+    }
+
+    $this_page_first_result = ($page-1)*$results_per_page;
+    // 이 페이지의 결과를 가져오는 SQL 쿼리
+    $sql="SELECT * FROM search WHERE name LIKE '%$search%' OR location LIKE '%$search%' OR type LIKE '%$search%' OR billtype LIKE '%$search%' LIMIT $this_page_first_result, $results_per_page";
     $result = $conn->query($sql);
 
-    if(isset($result) && $result->num_rows > 0){
+    if($total_results > 0){
         while($row = $result->fetch_assoc()){
-            echo "이름: ". $row['name']."<br>";
-            echo "지역: ". $row['location']."<br>";
-            echo "주거 형태: ". $row['type']."<br>";
-            echo "거래 형태: ". $row['billtype']."<br>";
-            echo "크기: ". $row['size']."㎡<hr><br>";;
+            echo "<strong>이름:</strong> ". $row['name']."<br>";
+            echo "<strong>지역:</strong> ". $row['location']."<br>";
+            echo "<strong>주거 형태:</strong> ". $row['type']."<br>";
+            echo "<strong>거래 형태:</strong> ". $row['billtype']."<br>";
+            echo "<strong>크기:</strong> ". $row['size']."㎡<hr><br>";
         }
     } else echo "검색된 데이터가 없습니다.";
 
+    echo '<div class="center">';
+
+    for ($page=1;$page<=$number_of_pages;$page++) {
+        echo '<a href="view.php?page=' . $page . '">' . $page . '</a> ';
+    }
+    echo '</div>';
     ?>
 </body>
 </html>
